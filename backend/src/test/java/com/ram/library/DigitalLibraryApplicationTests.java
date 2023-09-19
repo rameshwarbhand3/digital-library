33e3cc2aaad4b9bb6d191ca62570d2d4142ca298
rameshwarbhand3 <rameshwarbhand3@gmail.com>
2023-09-19 13:55:10 +0530

    Backend initial commit

---
+++ backend/src/test/java/com/ram/library/DigitalLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
